491ebab929509777d2e14289cd5ed2e86e97f748
huangwei4478 <huangwei4478@users.noreply.github.com>
2022-08-11 14:42:19 +0800

    [add] Kitty can rotate, changing her forward

--- Kitty3D/Assets/PlayerMovement.cs
+++ Kitty3D/Assets/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     private float m_vertial;
     private Animator m_animator;
     private Rigidbody m_rigidbody;
+    private float m_turnSpeed = 20.0f;
+    private Quaternion m_rotation = Quaternion.identity;
 
     // Start is called before the first frame update
     void Start()
@@ -35,10 +37,15 @@ public class PlayerMovement : MonoBehaviour
         bool isWalking = hasHorizontalMove || hasVerticalMove;
 
         m_animator.SetBool("isWalking", isWalking);
+
+        // 用三维矢量表示玩家的转动方向
+        Vector3 desiredRotation = Vector3.RotateTowards(transform.forward, m_movement, Time.deltaTime * m_turnSpeed, 0.0f);
+        m_rotation = Quaternion.LookRotation(desiredRotation);
     }
 
     private void OnAnimatorMove()
     {
         m_rigidbody.MovePosition(m_rigidbody.position + m_movement * m_animator.deltaPosition.magnitude);
+        m_rigidbody.MoveRotation(m_rotation);
     }
 }
