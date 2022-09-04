5a40df758e11561ead218f6c825a3f4d61aad103
huangwei4478 <huangwei4478@users.noreply.github.com>
2022-09-04 13:30:24 +0800

    [add] grid movement

--- TopDungeon/Assets/Scripts/Player.cs
+++ TopDungeon/Assets/Scripts/Player.cs
@@ -8,28 +8,76 @@ public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
 
-    private Vector3 moveDelta;
+    private SpriteRenderer spriteRenderer;
+
+    private bool isMoving;
+
+    private Vector3 origPos, targetPos;
+
+    private float timeToMove = 0.2f;            // 0.2 second for player to move from A to B
 
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        isMoving = false;
     }
 
     private void FixedUpdate()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
-        moveDelta = new Vector3(x, y, 0);
-
-        if (moveDelta.x > 0)
+        if (isMoving)
         {
-            transform.localScale = Vector3.one;
-        } else if (moveDelta.x < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1); 
-	    }
+            return;
+        }
 
-        transform.Translate(moveDelta * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            StartCoroutine(MovePlayer(Vector3.up));
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            StartCoroutine(MovePlayer(Vector3.left));
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            StartCoroutine(MovePlayer(Vector3.right));
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            StartCoroutine(MovePlayer(Vector3.down));
+        }
+
+
+     //   if (moveDelta.x > 0)
+     //   {
+     //       spriteRenderer.flipX = false;
+     //   } else if (moveDelta.x < 0)
+     //   {
+     //       spriteRenderer.flipX = true;
+	    //}
+        
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0;
+
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+        isMoving = false;
     }
 }
