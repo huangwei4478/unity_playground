d7e43257ea35b4801b07179ec477878fcaf31681
huangwei4478 <huangwei4478@users.noreply.github.com>
2022-09-04 11:46:15 +0800

    [add] import assets

---
+++ TopDungeon/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x, y, 0);
+
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        } else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1); 
+	    }
+
+        transform.Translate(moveDelta * Time.deltaTime);
+    }
+}
